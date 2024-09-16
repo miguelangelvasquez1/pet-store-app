20082fd552bcbb29a604602d8ba941f9c46a6eed
miguelangelvasquez1 <vgmiguel16@gmail.com>
2024-09-15 22:23:53 -0500

    creando el servicio CartServicesService

--- src/app/components/card-product/card-product.component.ts
+++ src/app/components/card-product/card-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-card-product',
@@ -6,18 +6,19 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrl: './card-product.component.scss'
 })
 export class CardProductComponent implements OnInit{
-// creacion del Input que recibe del componente padre el array de productos
   @Input() product = {
     name: '',
     price: 0,
     description: '',
     inventory: 0,
-    image: ''
+    image: '',
+    cantidad: 0
   };
+	//1. agregar output 
+  @Output() addToCart = new EventEmitter();
   
   cantidad: number = 0;
   soldOut: boolean = false;
-
 
   constructor() { }
 
@@ -40,4 +41,11 @@ export class CardProductComponent implements OnInit{
     }
   }
 
+  addToCartHandler() { //2. crear metodo
+    if(this.cantidad > 0){ //3. validar si tiena una cantidad de producto
+      this.product['cantidad'] = this.cantidad; //4. asigna la cantidad a perdir
+      this.addToCart.emit(this.product); //5. enviar la informacion por el event emiter del output
+    }
+    return null;
+  }
 }
--- src/app/components/catalogo/catalogo.component.ts
+++ src/app/components/catalogo/catalogo.component.ts
@@ -3,81 +3,111 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
-  styleUrl: './catalogo.component.css'
+  styleUrl: './catalogo.component.scss'
 })
+
+
 export class CatalogoComponent {
 
-	// Array de Objetos de productos
+  
+  // crea un método para añadir un producto al carrito
+  addToCart(product: any) { 
+    console.log(product)
+  }
+
+  // actulizar el array de productos con el atributo cantidad en cada producto
   products = [
     {
+      id:1,
       name: 'Bike_1',
       price: 120,
       description: 'Product Description',
       inventory: 10,
-      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-    }, 
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    },
     {
+      id:2,
       name: 'Bike_2',
       price: 120,
       description: 'Product Description',
       inventory: 10,
-      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
     },
     {
+      id:3,
       name: 'Bike_3',
       price: 120,
       description: 'Product Description',
       inventory: 10,
-      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
     }, 
     {
+      id:4,
       name: 'Bike_4',
       price: 120,
       description: 'Product Description',
       inventory: 10,
-      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
     },
     {
+      id:5,
       name: 'Bike_5',
       price: 120,
       description: 'Product Description',
       inventory: 10,
-      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
     }, 
     {
-      name: 'Bike',
-      price: 120,
-      description: 'Product Description',
-      inventory: 10,
-      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-    },
-    {
+      id:6,
       name: 'Bike_6',
       price: 120,
       description: 'Product Description',
       inventory: 10,
-      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-    }, 
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    },
     {
+      id:7,
       name: 'Bike_7',
       price: 120,
       description: 'Product Description',
       inventory: 10,
-      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-    },
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    }, 
     {
+      id:8,
       name: 'Bike_8',
       price: 120,
       description: 'Product Description',
       inventory: 10,
-      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-    }, 
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    },
     {
+      id:9,
       name: 'Bike_9',
       price: 120,
       description: 'Product Description',
       inventory: 10,
-      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-    },
- ]
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    }, 
+    {
+      id:19,
+      name: 'Bike_10',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    }, 
+    
+]
+
 }
